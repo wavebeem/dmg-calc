a088207b1b5dcc65e6933d441c8bafda24f7e87a
Brian Mock <brian@mockbrian.com>
2019-05-22 23:42:50 -0700

    nice

--- main.js
+++ main.js
@@ -5,13 +5,21 @@ const { Vue } = window;
 
 const app = new Vue({
   el: "#app",
+  filters: {
+    formatNumber(value) {
+      if (!value) {
+        return "–"; // ndash
+      }
+      return value.toFixed(3);
+    }
+  },
   computed: {
     damage() {
       return Number(this.inputDamage || 0);
     },
 
     accuracy() {
-      return Number(this.inputAccuracy || 0);
+      return Number(this.inputAccuracy || 0) / 100;
     },
 
     minimumHits() {
@@ -26,12 +34,37 @@ const app = new Vue({
       return Number(this.inputCost || 0);
     },
 
+    isFormComplete() {
+      return (
+        this.inputDamage &&
+        this.inputAccuracy &&
+        this.inputMinimumHits &&
+        this.inputMaximumHits &&
+        this.inputCost
+      );
+    },
+
+    averageHits() {
+      if (this.isFormComplete) {
+        if (this.minimumHits === this.maximumHits) {
+          return this.minimumHits;
+        }
+        const count = this.maximumHits - this.minimumHits;
+        const sum = (count / 2) * (this.minimumHits + this.maximumHits);
+        return sum / count;
+      }
+      return "";
+    },
+
     averageDamage() {
+      if (this.isFormComplete) {
+        return this.damage * this.accuracy * this.averageHits;
+      }
       return "";
     },
 
     averageEfficiency() {
-      if (this.averageDamage && this.cost) {
+      if (this.isFormComplete && this.cost > 0) {
         return this.averageDamage / this.cost;
       }
       return "";
@@ -42,7 +75,7 @@ const app = new Vue({
     },
 
     minimumEfficiency() {
-      if (this.minimumDamage && this.cost) {
+      if (this.isFormComplete && this.cost > 0) {
         return this.minimumDamage / this.cost;
       }
       return "";
@@ -53,7 +86,7 @@ const app = new Vue({
     },
 
     maximumEfficiency() {
-      if (this.maximumDamage && this.cost) {
+      if (this.isFormComplete && this.cost > 0) {
         return this.maximumDamage / this.cost;
       }
       return "";
@@ -61,21 +94,28 @@ const app = new Vue({
   },
   data: {
     css: {
-      label: "db mt3 mb1",
-      input: "db ba b--black-30 pa2 br1",
+      label: "db mt3 mb1 b",
+      input: "db ba bw1 b--dark-blue pa2 bg-lightest-blue navy",
       thead: "",
       tbody: "",
-      table: "ba b--black-30 br1 w-100 collapse mt3 bg-white",
-      tr: "ba b--black-30 tl",
-      td: "ba b--black-30 pa2 tr",
-      th: "ba b--black-30 pa2 tl bg-moon-gray"
+      table: "w-100 collapse mt3 bg-lightest-blue navy",
+      tr: "ba bw1 b--dark-blue tl",
+      td: "ba bw1 b--dark-blue pa2 tr",
+      th: "ba bw1 b--dark-blue pa2 tl bg-light-blue"
     },
 
-    inputDamage: "",
-    inputAccuracy: "",
-    inputMinimumHits: "",
-    inputMaximumHits: "",
-    inputCost: ""
+    // inputDamage: "0",
+    // inputAccuracy: "100",
+    // inputMinimumHits: "1",
+    // inputMaximumHits: "1",
+    // inputCost: "0"
+
+    // Temporarily use the Zodiac lv.1 Meteor spell
+    inputDamage: "50",
+    inputAccuracy: "95",
+    inputMinimumHits: "2",
+    inputMaximumHits: "5",
+    inputCost: "27"
   }
 });
 
